6e91f18b3c63a834d918d9dd7b134d593591ce9e
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2024-04-21 22:01:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+function App() {
+
+  //  <style>
+  //     .attribution {
+  //       font-size: 11px;
+  //       text-align: center;
+  //     }
+  //     .attribution a {
+  //       color: hsl(228, 45%, 44%);
+  //     }
+  //   </style>
+  return (
+    <div className="App">
+      <div className="card">
+        <div className="card-content">
+          <img src="src/assets/images/illustration-article.svg" alt="blog-image" className="blog-image" />
+          <button className="btn"> Learning </button>
+          <p className="card-date">Published 21 Dec 2023</p> 
+          <p className="card-title">HTML & CSS foundations </p>
+          <p className="card-desc">These languages are
+            the backbone of every website, defining structure, content, and
+            presentation. 
+          </p>
+          <div className="card-author">
+            <img src="src/assets/images/profile_pic.png" alt="profile-pic" width={32} />
+            <p> Nikhil Bhaladhare</p>
+          </div>
+     
+         
+
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
